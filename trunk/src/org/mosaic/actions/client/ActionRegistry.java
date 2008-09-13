0062f532031edcf5b882ad766296222ce5194eba
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-09-13 13:26:32 +0000

    Actions update

---
+++ trunk/src/org/mosaic/actions/client/ActionRegistry.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2008 Georgios J. Georgopoulos.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.mosaic.actions.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionRegistry {
+
+  private static final Map<String, Action> actionMap = new HashMap<String, Action>();
+
+  public static Action get(String id) {
+    return actionMap.get(id);
+  }
+
+  public static void register(Action action) {
+    actionMap.put(action.getId(), action);
+  }
+
+  public static void unregister(Action action) {
+    actionMap.remove(action.getId());
+  }
+}
