4f2c922034cc5fc86d88342f34332b65d5be16dd
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-09-29 13:53:02 +0000

    ComboBox update

--- trunk/src/org/gwt/mosaic/ui/client/ComboBox.java
+++ trunk/src/org/gwt/mosaic/ui/client/ComboBox.java
@@ -15,27 +15,42 @@
  */
 package org.gwt.mosaic.ui.client;
 
+import org.gwt.mosaic.core.client.util.DelayedRunnable;
 import org.gwt.mosaic.ui.client.layout.BoxLayout;
 import org.gwt.mosaic.ui.client.layout.BoxLayoutData;
 import org.gwt.mosaic.ui.client.layout.LayoutPanel;
 import org.gwt.mosaic.ui.client.layout.BoxLayout.Orientation;
 import org.gwt.mosaic.ui.client.layout.BoxLayoutData.FillStyle;
 
-import com.google.gwt.dom.client.InputElement;
-import com.google.gwt.i18n.client.HasDirection.Direction;
+import com.google.gwt.i18n.client.HasDirection;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FocusListener;
+import com.google.gwt.user.client.ui.HasFocus;
 import com.google.gwt.user.client.ui.HasName;
 import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.KeyboardListener;
+import com.google.gwt.user.client.ui.PopupListener;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.SourcesChangeEvents;
+import com.google.gwt.user.client.ui.SourcesClickEvents;
+import com.google.gwt.user.client.ui.SourcesFocusEvents;
+import com.google.gwt.user.client.ui.SourcesKeyboardEvents;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public abstract class ComboBox extends LayoutComposite implements HasName, HasText {
+public abstract class ComboBox<T extends Widget> extends LayoutComposite
+    implements HasFocus, HasDirection, HasName, HasText, SourcesClickEvents,
+    SourcesChangeEvents, SourcesFocusEvents, SourcesKeyboardEvents {
 
   private static final String DEFAULT_STYLENAME = "mosaic-ComboBox";
 
-  protected final TextBox input;
-  protected final Button button;
+  private final TextBox input;
+  private final Button button;
+  private final DropDownPanel popup;
+
+  private boolean cancelNextClick;
 
   public ComboBox() {
     this(DEFAULT_STYLENAME);
@@ -43,36 +58,113 @@ public abstract class ComboBox extends LayoutComposite implements HasName, HasTe
 
   protected ComboBox(String styleName) {
     super();
-    final LayoutPanel layoutPanel = getWidget();
+    final LayoutPanel layoutPanel = super.getWidget();
     layoutPanel.setLayout(new BoxLayout(Orientation.HORIZONTAL));
     layoutPanel.setPadding(0);
     layoutPanel.setWidgetSpacing(0);
 
     input = new TextBox();
     layoutPanel.add(input, new BoxLayoutData(FillStyle.BOTH));
+    input.addKeyboardListener(new KeyboardListener() {
+      public void onKeyDown(Widget sender, char keyCode, int modifiers) {
+        switch (keyCode) {
+          case KEY_ENTER:
+          case KEY_TAB:
+          case KEY_ESCAPE:
+            hidePopup();
+            break;
+          case KEY_DOWN:
+            if (!popup.isAttached()) {
+              new DelayedRunnable(1) {
+                @Override
+                public void run() {
+                  showPopup();
+                }
+              };
+            }
+            break;
+          default:
+            // dirtyText = false;
+        }
+      }
 
-    button = new Button("&nbsp;", new ClickListener() {
+      public void onKeyPress(Widget sender, char keyCode, int modifiers) {
+        // Nothing to do here!
+      }
+
+      public void onKeyUp(Widget sender, char keyCode, int modifiers) {
+        // Nothing to do here!
+      }
+
+    });
+
+    button = new Button();
+    button.addClickListener(new ClickListener() {
       public void onClick(Widget sender) {
-        onButtonClick();
+        if (cancelNextClick) {
+          cancelNextClick = false;
+        } else {
+          showPopup();
+        }
       }
     });
     layoutPanel.add(button, new BoxLayoutData(FillStyle.VERTICAL));
 
+    popup = new DropDownPanel(this);
+    popup.addPopupListener(new PopupListener() {
+      public void onPopupClosed(PopupPanel sender, boolean autoClosed) {
+        if (!autoClosed) {
+          input.setFocus(true);
+        }
+      }
+    });
+
     setStyleName(styleName);
   }
-  
-  protected abstract void onButtonClick();
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesChangeEvents#addChangeListener(com.google.gwt.user.client.ui.ChangeListener)
+   */
+  public void addChangeListener(ChangeListener listener) {
+    input.addChangeListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesClickEvents#addClickListener(com.google.gwt.user.client.ui.ClickListener)
+   */
+  public void addClickListener(ClickListener listener) {
+    input.addClickListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesFocusEvents#addFocusListener(com.google.gwt.user.client.ui.FocusListener)
+   */
+  public void addFocusListener(FocusListener listener) {
+    input.addFocusListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesKeyboardEvents#addKeyboardListener(com.google.gwt.user.client.ui.KeyboardListener)
+   */
+  public void addKeyboardListener(KeyboardListener listener) {
+    input.addKeyboardListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.i18n.client.HasDirection#getDirection()
+   */
   public Direction getDirection() {
     return input.getDirection();
-  }
-
-  public void setDirection(Direction direction) {
-    input.setDirection(direction);
-  }
-
-  protected InputElement getInputElement() {
-    return getElement().cast();
   }
 
   /*
@@ -87,10 +179,10 @@ public abstract class ComboBox extends LayoutComposite implements HasName, HasTe
   /*
    * (non-Javadoc)
    * 
-   * @see com.google.gwt.user.client.ui.HasName#setName(java.lang.String)
+   * @see com.google.gwt.user.client.ui.HasFocus#getTabIndex()
    */
-  public void setName(String name) {
-    input.setName(name);
+  public int getTabIndex() {
+    return input.getTabIndex();
   }
 
   /*
@@ -102,13 +194,24 @@ public abstract class ComboBox extends LayoutComposite implements HasName, HasTe
     return input.getText();
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see com.google.gwt.user.client.ui.HasText#setText(java.lang.String)
-   */
-  public void setText(String text) {
-    input.setText(text);
+  protected void hidePopup() {
+    hidePopup(false);
+  }
+  
+  protected boolean isPopupVisible() {
+    return popup.isAttached();
+  }
+
+  protected void hidePopup(boolean autoHide) {
+    if (autoHide) {
+      popup.hide(true);
+    } else if (onHidePopup()) {
+      popup.hide();
+    }
+  }
+
+  public boolean isAnimationEnabled() {
+    return popup.isAnimationEnabled();
   }
 
   /**
@@ -121,11 +224,97 @@ public abstract class ComboBox extends LayoutComposite implements HasName, HasTe
     return input.isReadOnly();
   }
 
+  @Override
+  public void layout() {
+    hidePopup(true);
+    super.layout();
+  }
+
+  protected abstract T onShowPopup();
+
+  protected abstract boolean onHidePopup();
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesChangeEvents#removeChangeListener(com.google.gwt.user.client.ui.ChangeListener)
+   */
+  public void removeChangeListener(ChangeListener listener) {
+    input.removeChangeListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesClickEvents#removeClickListener(com.google.gwt.user.client.ui.ClickListener)
+   */
+  public void removeClickListener(ClickListener listener) {
+    input.removeClickListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesFocusEvents#removeFocusListener(com.google.gwt.user.client.ui.FocusListener)
+   */
+  public void removeFocusListener(FocusListener listener) {
+    input.removeFocusListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.SourcesKeyboardEvents#removeKeyboardListener(com.google.gwt.user.client.ui.KeyboardListener)
+   */
+  public void removeKeyboardListener(KeyboardListener listener) {
+    input.removeKeyboardListener(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasFocus#setAccessKey(char)
+   */
+  public void setAccessKey(char key) {
+    input.setAccessKey(key);
+  }
+
+  public void setAnimationEnabled(boolean enable) {
+    popup.setAnimationEnabled(enable);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.i18n.client.HasDirection#setDirection(com.google.gwt.i18n.client.HasDirection.Direction)
+   */
+  public void setDirection(Direction direction) {
+    input.setDirection(direction);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasFocus#setFocus(boolean)
+   */
+  public void setFocus(boolean focused) {
+    input.setFocus(focused);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasName#setName(java.lang.String)
+   */
+  public void setName(String name) {
+    input.setName(name);
+  }
+
   /**
    * Turns read-only mode on or off.
    * 
    * @param readOnly if <code>true</code>, the widget becomes read-only; if
-   *            <code>false</code> the widget becomes editable
+   *          <code>false</code> the widget becomes editable
    */
   public void setReadOnly(boolean readOnly) {
     input.setReadOnly(readOnly);
@@ -134,6 +323,38 @@ public abstract class ComboBox extends LayoutComposite implements HasName, HasTe
       addStyleDependentName(readOnlyStyle);
     } else {
       removeStyleDependentName(readOnlyStyle);
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasFocus#setTabIndex(int)
+   */
+  public void setTabIndex(int index) {
+    input.setTabIndex(index);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasText#setText(java.lang.String)
+   */
+  public void setText(String text) {
+    input.setText(text);
+  }
+
+  protected void showPopup() {
+    if (popup.isAttached()) {
+      hidePopup(true);
+    } else {
+      final T w = onShowPopup();
+      if (w != null) {
+        if (w != popup.getWidget()) {
+          popup.setWidget(w);
+        }
+        popup.show();
+      }
     }
   }
 
