0726699394ac37b0626dd5129cf7e76135100bf1
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-10-03 20:22:04 +0000

    Copyright changed from: Copyright 2008 Georgios J. Georgopoulos, to: Copyright 2008 Google Inc.

--- src/org/gwt/mosaic/ui/client/LayoutComposite.java
+++ src/org/gwt/mosaic/ui/client/LayoutComposite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Georgios J. Georgopoulos.
+ * Copyright 2008 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -20,6 +20,10 @@ import org.gwt.mosaic.ui.client.layout.LayoutPanel;
 
 import com.google.gwt.user.client.ui.Composite;
 
+/**
+ * 
+ * @author georgopoulos.georgios(at)gmail.com
+ */
 public abstract class LayoutComposite extends Composite implements HasLayoutManager {
 
   /**
