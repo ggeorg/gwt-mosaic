44cd29a7c2378bcf82190016df8301e36c3cc892
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-10-25 20:48:34 +0000

    0.1.3-1

---
+++ tags/0.1.3-1/src/org/gwt/mosaic/showcase/client/content/layout/CwBorderLayout.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gwt.mosaic.showcase.client.content.layout;
+
+import org.gwt.mosaic.showcase.client.ContentWidget;
+import org.gwt.mosaic.showcase.client.ShowcaseAnnotations.ShowcaseSource;
+import org.gwt.mosaic.showcase.client.ShowcaseAnnotations.ShowcaseStyle;
+import org.gwt.mosaic.ui.client.layout.BorderLayout;
+import org.gwt.mosaic.ui.client.layout.BorderLayoutData;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
+import org.gwt.mosaic.ui.client.layout.BorderLayout.BorderLayoutRegion;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Example file.
+ * 
+ * @author georgopoulos.georgios(at)gmail.com
+ */
+@ShowcaseStyle({".mosaic-LayoutPanel"})
+public class CwBorderLayout extends ContentWidget {
+
+  /**
+   * Constructor.
+   * 
+   * @param constants the constants
+   */
+  public CwBorderLayout(CwConstants constants) {
+    super(constants);
+  }
+  
+  @Override
+  public String getDescription() {
+    return "BorderLayout description";
+  }
+
+  @Override
+  public String getName() {
+    return "BorderLayout";
+  }
+
+  /**
+   * Initialize this example.
+   */
+  @ShowcaseSource
+  @Override
+  protected Widget onInitialize() {
+    // Create a layout panel to align the widgets
+    final LayoutPanel layoutPanel = new LayoutPanel(new BorderLayout());
+    
+    final Button b1 = new Button("Button 1");
+    final Button b2 = new Button("Button 2");
+    final Button b3 = new Button("Button 3");
+    final Button b4 = new Button("Button 4");
+    final Button b5 = new Button("Button 5");
+    
+    layoutPanel.add(b1, new BorderLayoutData(BorderLayoutRegion.NORTH, 10, 200));
+    layoutPanel.add(b2, new BorderLayoutData(BorderLayoutRegion.SOUTH, 10, 200));
+    layoutPanel.add(b3, new BorderLayoutData(BorderLayoutRegion.WEST, 10, 200));
+    layoutPanel.add(b4, new BorderLayoutData(BorderLayoutRegion.EAST, 10, 200));
+    layoutPanel.add(b5, new BorderLayoutData(BorderLayoutRegion.CENTER, true));
+    
+    return layoutPanel;
+  }
+  
+}
---
+++ tags/0.1.3-1/src/org/gwt/mosaic/showcase/client/content/layout/CwNestedBorderLayout.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2008 Georgios J. Georgopoulos
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gwt.mosaic.showcase.client.content.layout;
+
+import org.gwt.mosaic.showcase.client.ContentWidget;
+import org.gwt.mosaic.showcase.client.ShowcaseAnnotations.ShowcaseSource;
+import org.gwt.mosaic.showcase.client.ShowcaseAnnotations.ShowcaseStyle;
+import org.gwt.mosaic.ui.client.layout.BorderLayout;
+import org.gwt.mosaic.ui.client.layout.BorderLayoutData;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
+import org.gwt.mosaic.ui.client.layout.BorderLayout.BorderLayoutRegion;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Example file.
+ */
+@ShowcaseStyle( {".mosaic-LayoutPanel"})
+public class CwNestedBorderLayout extends ContentWidget {
+
+  /**
+   * Constructor.
+   * 
+   * @param constants the constants
+   */
+  public CwNestedBorderLayout(CwConstants constants) {
+    super(constants);
+  }
+  
+  @Override
+  public String getDescription() {
+    return "Nested BorderLayout description";
+  }
+  
+  @Override
+  public String getName() {
+    return "Nested BorderLayout";
+  }
+
+  /**
+   * Initialize this example.
+   */
+  @ShowcaseSource
+  @Override
+  protected Widget onInitialize() {
+    // Create a layout panel to align the widgets
+    final LayoutPanel layoutPanel = new LayoutPanel(new BorderLayout());
+
+    final Button b1 = new Button("Button 1");
+    final Button b2 = new Button("Button 2");
+    final Button b3 = new Button("Button 3");
+    final Button b4 = new Button("Button 4");
+
+    final LayoutPanel layoutPanel1 = new LayoutPanel(new BorderLayout());
+
+    layoutPanel.add(b1, new BorderLayoutData(BorderLayoutRegion.NORTH, 10, 200));
+    layoutPanel.add(b2, new BorderLayoutData(BorderLayoutRegion.SOUTH, 10, 200));
+    layoutPanel.add(b3, new BorderLayoutData(BorderLayoutRegion.WEST, 10, 200));
+    layoutPanel.add(b4, new BorderLayoutData(BorderLayoutRegion.EAST, 10, 200));
+    layoutPanel.add(layoutPanel1, new BorderLayoutData(BorderLayoutRegion.CENTER, true));
+
+    final Button b11 = new Button("Button 11");
+    final Button b12 = new Button("Button 12");
+    final Button b13 = new Button("Button 13");
+    final Button b14 = new Button("Button 14");
+    final Button b15 = new Button("Button 15");
+
+    layoutPanel1.add(b11, new BorderLayoutData(BorderLayoutRegion.NORTH));
+    layoutPanel1.add(b12, new BorderLayoutData(BorderLayoutRegion.SOUTH));
+    layoutPanel1.add(b13, new BorderLayoutData(BorderLayoutRegion.WEST));
+    layoutPanel1.add(b14, new BorderLayoutData(BorderLayoutRegion.EAST));
+    layoutPanel1.add(b15, new BorderLayoutData(BorderLayoutRegion.CENTER));
+    
+    return layoutPanel;
+  }
+
+}
