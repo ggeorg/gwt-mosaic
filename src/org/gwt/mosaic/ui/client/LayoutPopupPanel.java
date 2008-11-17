f4bc895314b556d65a9fca80a9f6fabe53f30008
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-11-17 11:12:42 +0000

    New widgets: LayoutPopupPanel & DecoratedLayoutPanel

---
+++ src/org/gwt/mosaic/ui/client/LayoutPopupPanel.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gwt.mosaic.ui.client;
+
+import org.gwt.mosaic.core.client.DOM;
+import org.gwt.mosaic.ui.client.layout.HasLayoutManager;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * 
+ * @author georgopoulos.georgios(at)gmail.com
+ */
+public class LayoutPopupPanel extends PopupPanel implements HasLayoutManager {
+
+  private final LayoutPanel layoutPanel;
+
+  private final Command renderCmd = new Command() {
+    public void execute() {
+      afterLoad();
+    }
+  };
+
+  /**
+   * Creates an empty popup panel. A child widget must be added to it before it
+   * is shown.
+   */
+  public LayoutPopupPanel() {
+    this(false, false);
+  }
+
+  /**
+   * Creates an empty popup panel, specifying its "auto-hide" property.
+   * 
+   * @param autoHide <code>true</code> if the popup should be automatically
+   *          hidden when the user clicks outside of it
+   */
+  public LayoutPopupPanel(boolean autoHide) {
+    this(autoHide, false);
+  }
+
+  /**
+   * Creates an empty popup panel, specifying its "auto-hide" and "modal"
+   * properties.
+   * 
+   * @param autoHide <code>true</code> if the popup should be automatically
+   *          hidden when the user clicks outside of it
+   * @param modal <code>true</code> if keyboard or mouse events that do not
+   *          target the PopupPanel or its children should be ignored
+   */
+  public LayoutPopupPanel(boolean autoHide, boolean modal) {
+    super(autoHide, modal);
+
+    layoutPanel = new LayoutPanel();
+    layoutPanel.setPadding(0);
+    super.setWidget(layoutPanel);
+  }
+
+  @Override
+  public Widget getWidget() {
+    if (layoutPanel.getWidgetCount() > 0) {
+      return layoutPanel.getWidget(0);
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public void setWidget(Widget w) {
+    layoutPanel.clear();
+    layoutPanel.add(w);
+  }
+
+  @Override
+  protected void onLoad() {
+    DeferredCommand.addCommand(renderCmd);
+    // afterLoad();
+  }
+
+  protected LayoutPanel getLayoutPanel() {
+    return layoutPanel;
+  }
+
+  protected void setContentSize(int width, int height) {
+    DOM.setContentAreaWidth(layoutPanel.getElement(), width);
+    DOM.setContentAreaHeight(layoutPanel.getElement(), height);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.gwt.mosaic.ui.client.layout.HasLayoutManager#getPreferredSize()
+   */
+  public int[] getPreferredSize() {
+    return layoutPanel.getPreferredSize();
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.gwt.mosaic.ui.client.layout.HasLayoutManager#layout()
+   */
+  public void layout() {
+    layoutPanel.layout();
+
+    DeferredCommand.addCommand(new Command() {
+      public void execute() {
+        System.out.println(getOffsetWidth() + "x" + getOffsetHeight());
+      }
+    });
+  }
+
+  private int decorationWidthCache = 0;
+  private int decorationHeightCache = 0;
+
+  protected void afterLoad() {
+    if (desiredWidth != null && desiredHeight != null) {
+      layoutPanel.setSize(desiredWidth, desiredHeight);
+      final int[] box = DOM.getClientSize(getElement());
+      final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+      final int w = size[0] - (box[0] - size[0]);
+      final int h = size[1] - (box[1] - size[1]);
+      setContentSize(w, h);
+    } else if (desiredWidth != null) {
+      layoutPanel.setWidth(desiredWidth);
+      final int[] box = DOM.getClientSize(getElement());
+      final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+      final int w = size[0] - (box[0] - size[0]);
+      final int h = size[1];
+      setContentSize(w, h);
+    } else if (desiredHeight != null) {
+      layoutPanel.setHeight(desiredHeight);
+      final int[] box = DOM.getClientSize(getElement());
+      final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+      final int w = size[0];
+      final int h = size[1] - (box[1] - size[1]);
+      setContentSize(w, h);
+    } else {
+      final int[] size = layoutPanel.getPreferredSize();
+      setContentSize(size[0], size[1]);
+    }
+
+    layout();
+
+    final int[] box = DOM.getClientSize(getElement());
+    final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+    decorationWidthCache = (box[0] - size[0]);
+    decorationHeightCache = (box[1] - size[1]);
+  }
+
+  private String desiredHeight = null;
+
+  @Override
+  public void setHeight(String height) {
+    super.setHeight(height);
+    if (!isAttached()) {
+      this.desiredHeight = height;
+    } else {
+      layoutPanel.setHeight(height);
+      final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+      setContentSize(size[0], size[1] - decorationHeightCache);
+    }
+  }
+
+  private String desiredWidth = null;
+
+  @Override
+  public void setWidth(String width) {
+    super.setWidth(width);
+    if (!isAttached()) {
+      this.desiredWidth = width;
+    } else {
+      layoutPanel.setWidth(width);
+      final int[] size = DOM.getBoxSize(layoutPanel.getElement());
+      setContentSize(size[0] - decorationWidthCache, size[1]);
+    }
+  }
+}
