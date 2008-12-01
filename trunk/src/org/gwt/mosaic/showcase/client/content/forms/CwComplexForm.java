708a3f1a310a6b212a47e46d45715aee7018fc98
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2008-12-01 15:53:12 +0000

    FormLayout widget based showcase demo update

--- trunk/src/org/gwt/mosaic/showcase/client/content/forms/CwComplexForm.java
+++ trunk/src/org/gwt/mosaic/showcase/client/content/forms/CwComplexForm.java
@@ -23,11 +23,9 @@ import org.gwt.mosaic.showcase.client.ShowcaseAnnotations.ShowcaseStyle;
 import org.gwt.mosaic.ui.client.DecoratedTabLayoutPanel;
 import org.gwt.mosaic.ui.client.ScrollLayoutPanel;
 import org.gwt.mosaic.ui.client.datepicker.DateComboBox;
-import org.gwt.mosaic.ui.client.layout.BoxLayoutData;
-import org.gwt.mosaic.ui.client.layout.GridLayout;
+import org.gwt.mosaic.ui.client.layout.FormLayout;
 import org.gwt.mosaic.ui.client.layout.GridLayoutData;
 import org.gwt.mosaic.ui.client.layout.LayoutPanel;
-import org.gwt.mosaic.ui.client.layout.BoxLayoutData.FillStyle;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Command;
@@ -76,9 +74,11 @@ public class CwComplexForm extends ContentWidget {
     final DecoratedTabLayoutPanel tabPanel = new DecoratedTabLayoutPanel();
 
     final LayoutPanel form1 = new ScrollLayoutPanel();
+    form1.setPadding(0);
     tabPanel.add(form1, "Form #1");
 
     final LayoutPanel form2 = new ScrollLayoutPanel();
+    form2.setPadding(0);
     tabPanel.add(form2, "Form #2");
 
     // lazy form creation loading
@@ -90,7 +90,7 @@ public class CwComplexForm extends ContentWidget {
       public void onTabSelected(SourcesTabEvents sender, int tabIndex) {
         if (tabIndex == 0) {
           if (form1.getWidgetCount() == 0) {
-            form1.add(createForm1(), new BoxLayoutData(FillStyle.HORIZONTAL));
+            form1.add(createForm1());
             DeferredCommand.addCommand(new Command() {
               public void execute() {
                 form1.layout();
@@ -99,7 +99,7 @@ public class CwComplexForm extends ContentWidget {
           }
         } else if (tabIndex == 1) {
           if (form2.getWidgetCount() == 0) {
-            form2.add(createForm2(), new BoxLayoutData(FillStyle.HORIZONTAL));
+            form2.add(createForm2());
             DeferredCommand.addCommand(new Command() {
               public void execute() {
                 form2.layout();
@@ -123,12 +123,9 @@ public class CwComplexForm extends ContentWidget {
   @ShowcaseSource
   private Widget createForm1() {
     // Create a layout panel to align the widgets
-    final LayoutPanel formLayout = new LayoutPanel(new GridLayout(7, 7)) {
-      protected void onLoad() {
-        super.onLoad();
-        layout();
-      }
-    };
+    final LayoutPanel formLayout = new LayoutPanel(new FormLayout(
+        new double[] {-1.0, -2.0, -2.0, 5.0, -1.0, -2.0, -2.0}, new double[] {
+            -1.0, -1.0, -1.0, -1.0, -1.0, -1.0}));
 
     // Create and configure a Builder
     final PanelBuilder builder = new PanelBuilder(new FormLayoutPanel() {
@@ -175,12 +172,9 @@ public class CwComplexForm extends ContentWidget {
   @ShowcaseSource
   private Widget createForm2() {
     // Create a layout panel to align the widgets
-    final LayoutPanel formLayout = new LayoutPanel(new GridLayout(9, 10)) {
-      protected void onLoad() {
-        super.onLoad();
-        layout();
-      }
-    };
+    final LayoutPanel formLayout = new LayoutPanel(new FormLayout(
+        new double[] {-1.0, -2.0, -2.0, 5.0, -1.0, -2.0, -2.0}, new double[] {
+            -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0}));
 
     // Create and configure a Builder
     final PanelBuilder builder = new PanelBuilder(new FormLayoutPanel() {
@@ -196,38 +190,38 @@ public class CwComplexForm extends ContentWidget {
 
     // row #1
 
-    builder.addSeparator("Description", 9);
+    builder.addSeparator("Description", 7);
 
     // row #2
 
-    builder.add(new TextArea(), new GridLayoutData(7, 2));
+    builder.add(new TextArea(), new GridLayoutData(5, 2));
     builder.add(new CheckBox("Done"), new GridLayoutData(2, 2));
 
     // row #3
 
-    builder.addSeparator("Date / Time", 4);
+    builder.addSeparator("Date / Time", 3);
     builder.addGap();
-    builder.addSeparator("Attributes", 4);
+    builder.addSeparator("Attributes", 3);
 
     // row #4
 
     DateComboBox startDate = new DateComboBox();
     startDate.setDateFormat(DateTimeFormat.getFormat("dd-MM-yyyy"));
 
-    builder.addLabel("Start Date:", 2);
+    builder.addLabel("Start Date:", 1);
     builder.add(startDate, 2);
     builder.addGap();
-    builder.add(new CheckBox("Confidential"), 4);
+    builder.add(new CheckBox("Confidential"), 3);
 
     // row #5
 
     DateComboBox deadline = new DateComboBox();
     deadline.setDateFormat(DateTimeFormat.getFormat("dd-MM-yyyy"));
 
-    builder.addLabel("Deadline:", 2);
+    builder.addLabel("Deadline:", 1);
     builder.add(deadline, 2);
     builder.addGap();
-    builder.add(new CheckBox("Not related to work"), 4);
+    builder.add(new CheckBox("Not related to work"), 3);
 
     // row #6
 
@@ -236,28 +230,28 @@ public class CwComplexForm extends ContentWidget {
     duration.addItem("00:30");
     duration.addItem("01:00");
 
-    builder.addLabel("Duration:", 2);
+    builder.addLabel("Duration:", 1);
     builder.add(duration, 2);
     builder.addGap();
-    builder.addSeparator("Priority", 4);
+    builder.addSeparator("Priority", 3);
 
     // row #7
 
-    builder.addGap(5);
+    builder.addGap(4);
 
     ListBox priority = new ListBox();
     priority.addItem("Without Priority");
     priority.addItem("Urgent");
 
-    builder.add(priority, 4);
+    builder.add(priority, 3);
 
     // row #8
 
-    builder.addSeparator("Completion status", 9);
+    builder.addSeparator("Completion status", 7);
 
     // row #9
 
-    builder.addLabel("Percentage Complete", 7);
+    builder.addLabel("Percentage Complete", 5);
 
     ListBox listCompletion = new ListBox();
     listCompletion.addItem("0%", "0");
