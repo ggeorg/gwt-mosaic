81cf06b5afe8c00e2660fc12c5d6729fd9b2b409
cbraid <cbraid@15e68a53-2852-0410-b422-3de62d02f58e>
2009-01-26 00:41:15 +0000

    delete so that it can be re-branched from trunk

--- branches/GWT-1.6/src/org/gwt/mosaic/showcase/client/content/other/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package org.gwt.mosaic.showcase.client.content.other;
-
-import java.util.List;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface CustomerService extends RemoteService {
-  List<Customer> getCustomers();
-}
