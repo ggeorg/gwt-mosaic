cc78d20ad8b7efd1b0a4cbbe45418818231d8d0d
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2009-02-12 22:17:19 +0000

    Javadoc updates

--- src/org/gwt/mosaic/forms/client/layout/Size.java
+++ src/org/gwt/mosaic/forms/client/layout/Size.java
@@ -52,14 +52,8 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * An interface that describes sizes as used by the {@link FormLayout}:
- * component measuring sizes, constant sizes with value and unit, and bounded
+ * widget measuring sizes, constant sizes with value and unit, and bounded
  * sizes that provide lower and upper bounds for a size.
- * <p>
- * You can find a motivation for the different <code>Size</code> types in the
- * Forms whitepaper that is part of the product documentation and that is
- * available online too, see <a
- * href="http://www.jgoodies.com/articles/forms.pdf" >
- * http://www.jgoodies.com/articles/forms.pdf</a>.
  * 
  * @author Karsten Lentzsch
  * @author georgopoulos.georgios(at)gmail.com
@@ -71,13 +65,13 @@ public interface Size {
 
   /**
    * Computes and returns this Size's maximum pixel size applied to the given
-   * list of components using the specified measures.
+   * list of widgets using the specified measures.
    * <p>
    * Invoked by {@link FormSpec} to determine the size of a column or row. This
    * method is not intended to be called by API users, and it uses API invisible
    * parameter types.
    * 
-   * @param layoutPanel the layout container
+   * @param layoutPanel the layout panel
    * @param widgets the list of components used to compute the size
    * @param minMeasure the measure that determines the minimum sizes
    * @param prefMeasure the measure that determines the preferred sizes
@@ -89,11 +83,10 @@ public interface Size {
       FormLayout.Measure defaultMeasure);
 
   /**
-   * Describes if this Size can be compressed, if container space gets scarce.
+   * Describes if this Size can be compressed, if layout panel space gets scarce.
    * Used by the FormLayout size computations in <code>#compressedSizes</code>
    * to check whether a column or row can be compressed or not.
    * <p>
-   * 
    * The ComponentSize <em>default</em> is compressible, as well as BoundedSizes
    * that are based on the <em>default</em> size.
    * 
