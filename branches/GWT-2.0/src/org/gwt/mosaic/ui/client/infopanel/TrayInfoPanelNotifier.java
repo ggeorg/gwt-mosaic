dc8b571f3cd9ce9d9a2ff7425a7d94cd8340f704
agarin <agarin@15e68a53-2852-0410-b422-3de62d02f58e>
2009-08-07 14:13:17 +0000

    removing deprecated GWT API call (PopupListener)

--- branches/GWT-2.0/src/org/gwt/mosaic/ui/client/infopanel/TrayInfoPanelNotifier.java
+++ branches/GWT-2.0/src/org/gwt/mosaic/ui/client/infopanel/TrayInfoPanelNotifier.java
@@ -20,8 +20,9 @@ import java.util.List;
 
 import org.gwt.mosaic.ui.client.infopanel.InfoPanel.InfoPanelType;
 
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.PopupListener;
 import com.google.gwt.user.client.ui.PopupPanel;
 /**
  * This is the default implementation for rendering system events in the right bottom of your application.
@@ -29,111 +30,112 @@ import com.google.gwt.user.client.ui.PopupPanel;
  *
  * @author luciano.broussal(at)gmail.com
  */
-public class TrayInfoPanelNotifier implements InfoPanelNotifier, PopupListener {
+public class TrayInfoPanelNotifier implements InfoPanelNotifier, CloseHandler<PopupPanel> {
 
-    //TODO Separate Humanized message from tray ones. no sense to have both in this class.
-    
-    List<InfoPanel> waintingInfoPanel = new ArrayList<InfoPanel>();
-    List<InfoPanel> visibleInfoPanel = new ArrayList<InfoPanel>();
-    int currentLevel = 1;
-    
-    private static TrayInfoPanelNotifier instance = null;
-    
-    public static TrayInfoPanelNotifier getInstance() {
-      if (instance == null) {
-        instance = new TrayInfoPanelNotifier();
+  //TODO Separate Humanized message from tray ones. no sense to have both in this class.
+
+  List<InfoPanel> waintingInfoPanel = new ArrayList<InfoPanel>();
+  List<InfoPanel> visibleInfoPanel = new ArrayList<InfoPanel>();
+  int currentLevel = 1;
+
+  private static TrayInfoPanelNotifier instance = null;
+
+  public static TrayInfoPanelNotifier getInstance() {
+    if (instance == null) {
+      instance = new TrayInfoPanelNotifier();
+    }
+    return instance;
+  }
+
+  public static void notifyTrayEvent(String caption , String text){
+    getInstance().show(InfoPanelType.TRAY_NOTIFICATION, caption, text);
+  }
+
+  public static void notifyModalEvent(String caption , String text){
+    getInstance().show(InfoPanelType.HUMANIZED_MESSAGE, caption, text);
+  }
+
+  /**
+   * Default constructor.
+   */
+  protected TrayInfoPanelNotifier() {
+    // Nothing to do here!
+  }
+
+  public void show(InfoPanelType infoPanelType, String caption, String content) {
+    if (infoPanelType == InfoPanelType.TRAY_NOTIFICATION) {
+      InfoPanel infoPanel = new InfoPanel(caption, content);
+
+      if (canLayoutInfoPanel(infoPanel)) {
+        showInfoPanel(infoPanel, currentLevel);
+        currentLevel++;
+        visibleInfoPanel.add(infoPanel);
+      } else {
+        visibleInfoPanel.add(infoPanel);
+        waintingInfoPanel.add(infoPanel);
       }
-      return instance;
-    }
-    
-    public static void notifyTrayEvent(String caption , String text){
-      getInstance().show(InfoPanelType.TRAY_NOTIFICATION, caption, text);
+      return;
     }
 
-    public static void notifyModalEvent(String caption , String text){
-      getInstance().show(InfoPanelType.HUMANIZED_MESSAGE, caption, text);
+    if(infoPanelType == InfoPanelType.HUMANIZED_MESSAGE){
+      final InfoPanel infoPanel = new InfoPanel(caption, content, true);
+      infoPanel.showModal();
+      return;
     }
-    
-    /**
-     * Default constructor.
-     */
-    protected TrayInfoPanelNotifier() {
-      // Nothing to do here!
+  }
+
+  private void showInfoPanel(InfoPanel infoPanel, int level) {
+    int[] coordinates = calculateInfoPanelPosition(infoPanel, level);
+    infoPanel.addCloseHandler(this);
+    infoPanel.setPopupPosition(coordinates[0], coordinates[1]);
+    infoPanel.show();
+  }
+
+  private boolean canLayoutInfoPanel(InfoPanel infoPanel) {
+    final int ch = Window.getClientHeight();
+    final int top = ch - InfoPanel.HEIGHT - 20 - (currentLevel * (InfoPanel.HEIGHT + 20));
+    return top >= 0;
+  }
+
+  private int[] calculateInfoPanelPosition(InfoPanel infoPanel, int level) {
+    final int windowWidth = Window.getClientWidth();
+    final int windowHeight = Window.getClientHeight();
+    final int left = (windowWidth - InfoPanel.WIDTH - 20);
+    final int top = windowHeight - (level * (InfoPanel.HEIGHT + 20));
+
+    return new int[] { left, top };
+
+  }
+
+  private void pushUpVisibleInfoPanels() {
+    for (int i = visibleInfoPanel.size() - 1; i >= 0; i--) {
+      InfoPanel infoPanel = visibleInfoPanel.get(i);
+      int[] newCoordinates = calculateInfoPanelPosition(infoPanel, i + 1);
+      infoPanel.setPopupPosition(newCoordinates[0], newCoordinates[1]);
     }
+  }
 
-    public void show(InfoPanelType infoPanelType, String caption, String content) {
-	if (infoPanelType == InfoPanelType.TRAY_NOTIFICATION) {
-	    InfoPanel infoPanel = new InfoPanel(caption, content);
-
-	    if (canLayoutInfoPanel(infoPanel)) {
-		showInfoPanel(infoPanel, currentLevel);
-		currentLevel++;
-		visibleInfoPanel.add(infoPanel);
-	    } else {
-		visibleInfoPanel.add(infoPanel);
-		waintingInfoPanel.add(infoPanel);
-	    }
-	    return;
-	}
-	
-	if(infoPanelType == InfoPanelType.HUMANIZED_MESSAGE){
-	    final InfoPanel infoPanel = new InfoPanel(caption, content, true);
-	    infoPanel.showModal();
-	    return;
-	}
+  private void pushDownVisibleInfoPanels() {
+    for (int i = 0; i < visibleInfoPanel.size(); i++) {
+      InfoPanel infoPanel = visibleInfoPanel.get(i);
+      int[] newCoordinates = calculateInfoPanelPosition(infoPanel, i + 1);
+      infoPanel.setPopupPosition(newCoordinates[0], newCoordinates[1]);
     }
+  }
 
-    private void showInfoPanel(InfoPanel infoPanel, int level) {
-	int[] coordinates = calculateInfoPanelPosition(infoPanel, level);
-	infoPanel.addPopupListener(this);
-	infoPanel.setPopupPosition(coordinates[0], coordinates[1]);
-	infoPanel.show();
+  @Override
+  public void onClose(CloseEvent<PopupPanel> event) {
+    visibleInfoPanel.remove(event.getSource());
+    currentLevel--;
+    if (visibleInfoPanel.size() > 0) {
+      pushDownVisibleInfoPanels();
     }
-
-    private boolean canLayoutInfoPanel(InfoPanel infoPanel) {
-	final int ch = Window.getClientHeight();
-	final int top = ch - InfoPanel.HEIGHT - 20 - (currentLevel * (InfoPanel.HEIGHT + 20));
-	return top >= 0;
-    }
-
-    private int[] calculateInfoPanelPosition(InfoPanel infoPanel, int level) {
-	final int windowWidth = Window.getClientWidth();
-	final int windowHeight = Window.getClientHeight();
-	final int left = (windowWidth - InfoPanel.WIDTH - 20);
-	final int top = windowHeight - (level * (InfoPanel.HEIGHT + 20));
-
-	return new int[] { left, top };
-
-    }
-
-    public void onPopupClosed(PopupPanel sender, boolean autoClosed) {
-	visibleInfoPanel.remove(sender);
-	currentLevel--;
-	if (visibleInfoPanel.size() > 0) {
-	    pushDownVisibleInfoPanels();
-	}
-	if (waintingInfoPanel.size() > 0) {
-	    pushUpVisibleInfoPanels();
-	    InfoPanel infoPanel = waintingInfoPanel.remove(0);
-	    showInfoPanel(infoPanel, currentLevel + 1);
-	    currentLevel++;
-	}
+    if (waintingInfoPanel.size() > 0) {
+      pushUpVisibleInfoPanels();
+      InfoPanel infoPanel = waintingInfoPanel.remove(0);
+      showInfoPanel(infoPanel, currentLevel + 1);
+      currentLevel++;
     }
 
-    private void pushUpVisibleInfoPanels() {
-	for (int i = visibleInfoPanel.size() - 1; i >= 0; i--) {
-	    InfoPanel infoPanel = visibleInfoPanel.get(i);
-	    int[] newCoordinates = calculateInfoPanelPosition(infoPanel, i + 1);
-	    infoPanel.setPopupPosition(newCoordinates[0], newCoordinates[1]);
-	}
-    }
-
-    private void pushDownVisibleInfoPanels() {
-	for (int i = 0; i < visibleInfoPanel.size(); i++) {
-	    InfoPanel infoPanel = visibleInfoPanel.get(i);
-	    int[] newCoordinates = calculateInfoPanelPosition(infoPanel, i + 1);
-	    infoPanel.setPopupPosition(newCoordinates[0], newCoordinates[1]);
-	}
-    }
-
+  }
 }
