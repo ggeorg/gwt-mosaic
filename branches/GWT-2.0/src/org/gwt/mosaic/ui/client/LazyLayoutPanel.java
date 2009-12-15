32d20e4791c4564e1f72b74ad5a8e8cb41846853
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2009-12-15 11:07:17 +0000

    (Bug Fix) LazyLayoutPanel layout problems fix

--- branches/GWT-2.0/src/org/gwt/mosaic/ui/client/LazyLayoutPanel.java
+++ branches/GWT-2.0/src/org/gwt/mosaic/ui/client/LazyLayoutPanel.java
@@ -19,6 +19,8 @@ import java.util.Iterator;
 
 import org.gwt.mosaic.ui.client.layout.LayoutPanel;
 
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -32,7 +34,8 @@ import com.google.gwt.user.client.ui.Widget;
  * 
  * @author georgopoulos.georgios(at)gmail.com
  */
-public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidgets {
+public abstract class LazyLayoutPanel extends LayoutComposite implements
+    HasWidgets {
 
   private Widget widget = null;
 
@@ -55,8 +58,14 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
    */
   public void ensureWidget() {
     if (widget == null) {
-      widget = createWidget();
-      getLayoutPanel().add(widget);
+      final LayoutPanel layoutPanel = getLayoutPanel();
+      layoutPanel.add(widget = createWidget());
+      layoutPanel.invalidate(widget);
+      DeferredCommand.addCommand(new Command() {
+        public void execute() {
+          layoutPanel.layout();
+        }
+      });
     }
   }
 
@@ -64,6 +73,7 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
    * Gets the panel's child widget.
    * 
    * @return the child widget, or {@code null} if none is present
+   * @see com.google.gwt.user.client.ui.Composite#getWidget()
    */
   @Override
   public Widget getWidget() {
@@ -73,7 +83,6 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
   /**
    * Sets whether this object is visible. If {@code visible} is {@code true},
    * creates the sole child widget if necessary by calling {@link #ensureWidget}
-   * .
    * 
    * @param visible {@code true} to show the object, {@code false} to hide it
    * @see com.google.gwt.user.client.ui.UIObject#setVisible(boolean)
@@ -90,6 +99,7 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
    * Adds a widget to this panel.
    * 
    * @param w the child widget to be added
+   * @see com.google.gwt.user.client.ui.HasWidgets#add(com.google.gwt.user.client.ui.Widget)
    */
   public void add(Widget w) {
     final LayoutPanel layoutPanel = getLayoutPanel();
@@ -100,15 +110,11 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
     getLayoutPanel().add(widget);
   }
 
-  public void clear() {
-    // TODO Auto-generated method stub
-
-  }
-
   /**
    * Return a simple iterator that enumerates the 0 or 1 elements in this panel.
    * 
    * @return the iterator
+   * @see com.google.gwt.user.client.ui.HasWidgets#iterator()
    */
   public Iterator<Widget> iterator() {
     return getLayoutPanel().iterator();
@@ -119,6 +125,7 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
    * 
    * @param w the widget to be removed
    * @return {@code true} if the child was present
+   * @see com.google.gwt.user.client.ui.HasWidgets#remove(com.google.gwt.user.client.ui.Widget)
    */
   public boolean remove(Widget w) {
     // Validate.
@@ -133,5 +140,14 @@ public abstract class LazyLayoutPanel extends LayoutComposite implements HasWidg
     widget = null;
 
     return true;
+  }
+
+  /**
+   * Removes all child widgets.
+   * 
+   * @see com.google.gwt.user.client.ui.HasWidgets#clear()
+   */
+  public void clear() {
+    remove(widget);
   }
 }
