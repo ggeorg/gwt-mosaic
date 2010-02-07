15bfa5c46b3b0bcdb7f9432e18680fa0bebbf91b
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2010-02-07 23:08:59 +0000

    (Update) Removed all GWT Incubator dependencies

--- src/org/gwt/mosaic/ui/client/impl/GlassPanelImplSafari.java
+++ src/org/gwt/mosaic/ui/client/impl/GlassPanelImplSafari.java
@@ -46,8 +46,8 @@ import com.google.gwt.user.client.Element;
 public class GlassPanelImplSafari extends GlassPanelImplStandard {
   private static native void implMatchDocumentSize(Element elem)
   /*-{
-    var scrollWidth = @com.google.gwt.widgetideas.client.impl.GlassPanelImpl::getWindowScrollWidth()();
-    var scrollHeight = @com.google.gwt.widgetideas.client.impl.GlassPanelImpl::getWindowScrollHeight()();
+    var scrollWidth = @org.gwt.mosaic.ui.client.impl.GlassPanelImpl::getWindowScrollWidth()();
+    var scrollHeight = @org.gwt.mosaic.ui.client.impl.GlassPanelImpl::getWindowScrollHeight()();
 
     var width = Math.max($wnd.innerWidth, scrollWidth);
     var height = Math.max($wnd.innerHeight, scrollHeight);
