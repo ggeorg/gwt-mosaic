9160e7ab08abe965f9b8969eb6f38415c4db89a3
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2010-02-11 19:32:09 +0000

    (Bug Fix) Bug: Should org.gwt.mosaic.showcase.client.content.tables.CwLiveTable$Foo be a _static_ inner class?

--- src/org/gwt/mosaic/showcase/client/content/tables/CwLiveTable.java
+++ src/org/gwt/mosaic/showcase/client/content/tables/CwLiveTable.java
@@ -68,7 +68,7 @@ public class CwLiveTable extends ContentWidget {
    * User object.
    */
   @ShowcaseSource
-  class Foo {
+  static class Foo {
     private static final long serialVersionUID = 1120309810337478764L;
 
     int[] values = new int[5];
