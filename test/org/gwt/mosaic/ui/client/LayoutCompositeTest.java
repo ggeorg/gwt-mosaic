a25a0af4b75b01f6c71c4d4e4e6619c671819264
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2010-02-15 17:24:24 +0000

    (Added) Tests for LayoutComposite, LayoutPopupPanel & DecoratedLayoutPopupPanel

---
+++ test/org/gwt/mosaic/ui/client/LayoutCompositeTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2010 GWT Mosaic Georgios J. Georgopoulos.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gwt.mosaic.ui.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Tests for {@link LayoutComposite}.
+ * 
+ * @author georgopoulos.georgios(at)gmail.com
+ * 
+ */
+public class LayoutCompositeTest extends GWTTestCase {
+
+  @Override
+  public String getModuleName() {
+    return "org.gwt.mosaic.UI";
+  }
+
+  /**
+   * This test is here to prevent a "No tests found" warning in Junit.
+   * 
+   * TODO: Remove this when testBrowserEvents is enabled
+   */
+  public void testNothing() {
+  }
+}
