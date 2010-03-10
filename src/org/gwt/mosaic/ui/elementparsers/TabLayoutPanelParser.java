d08d8c4a57dada3136fb8bd8bfd40dd44d7652e8
georgopoulos.georgios <georgopoulos.georgios@15e68a53-2852-0410-b422-3de62d02f58e>
2010-03-10 12:38:40 +0000

    (Added) Copyright message

--- src/org/gwt/mosaic/ui/elementparsers/TabLayoutPanelParser.java
+++ src/org/gwt/mosaic/ui/elementparsers/TabLayoutPanelParser.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010 GWT Mosaic Georgios J. Georgopoulos.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.gwt.mosaic.ui.elementparsers;
 
 import org.gwt.mosaic.ui.client.TabLayoutPanel;
@@ -11,6 +26,12 @@ import com.google.gwt.uibinder.elementparsers.HtmlInterpreter;
 import com.google.gwt.uibinder.rebind.UiBinderWriter;
 import com.google.gwt.uibinder.rebind.XMLElement;
 
+/**
+ * Parses {@link org.gwt.mosaic.ui.client.TabLayoutPanel} widgets.
+ * 
+ * @author georgopoulos.georgios(at)gmail.com
+ * 
+ */
 public class TabLayoutPanelParser implements ElementParser {
 
   private static class Children {
