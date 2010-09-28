b85518d1ebcd01afb0cb226b609485753958f462
georgopoulos.georgios <georgopoulos.georgios@gmail.com>
2010-09-28 14:57:51 +0000

    (Added) Mouse handlers to Text control

--- src/org/gwt/mosaic2g/client/scene/control/Text.java
+++ src/org/gwt/mosaic2g/client/scene/control/Text.java
@@ -73,6 +73,26 @@ import org.gwt.mosaic2g.client.MyClientBundle;
 import org.gwt.mosaic2g.client.scene.Control;
 import org.gwt.mosaic2g.client.scene.Show;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
+import com.google.gwt.event.dom.client.HasAllMouseHandlers;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasDoubleClickHandlers;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.dom.client.MouseWheelEvent;
+import com.google.gwt.event.dom.client.MouseWheelHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.Composite;
@@ -211,7 +231,8 @@ public class Text extends Control implements HasAlignment {
 
 	// -------------------------
 	private class LabelWidget extends Composite implements HasAlignment,
-			HasHTML, HasWordWrap {
+			HasHTML, HasWordWrap, HasClickHandlers, HasDoubleClickHandlers,
+			HasAllMouseHandlers {
 		private final SimplePanel div;
 		private final HTML htmlDiv;
 
@@ -282,6 +303,40 @@ public class Text extends Control implements HasAlignment {
 		public void setHeight(String height) {
 			super.setHeight(height);
 			htmlDiv.setHeight(height);
+		}
+
+		public HandlerRegistration addMouseDownHandler(MouseDownHandler handler) {
+			return addDomHandler(handler, MouseDownEvent.getType());
+		}
+
+		public HandlerRegistration addMouseUpHandler(MouseUpHandler handler) {
+			return addDomHandler(handler, MouseUpEvent.getType());
+		}
+
+		public HandlerRegistration addMouseOutHandler(MouseOutHandler handler) {
+			return addDomHandler(handler, MouseOutEvent.getType());
+		}
+
+		public HandlerRegistration addMouseOverHandler(MouseOverHandler handler) {
+			return addDomHandler(handler, MouseOverEvent.getType());
+		}
+
+		public HandlerRegistration addMouseMoveHandler(MouseMoveHandler handler) {
+			return addDomHandler(handler, MouseMoveEvent.getType());
+		}
+
+		public HandlerRegistration addMouseWheelHandler(
+				MouseWheelHandler handler) {
+			return addDomHandler(handler, MouseWheelEvent.getType());
+		}
+
+		public HandlerRegistration addDoubleClickHandler(
+				DoubleClickHandler handler) {
+			return addHandler(handler, DoubleClickEvent.getType());
+		}
+
+		public HandlerRegistration addClickHandler(ClickHandler handler) {
+			return addDomHandler(handler, ClickEvent.getType());
 		}
 
 	}
