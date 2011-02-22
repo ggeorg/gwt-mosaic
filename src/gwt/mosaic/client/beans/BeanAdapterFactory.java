93303d1106806c1cb88e4d9e9d9361209a1e0a5e
georgopoulos.georgios <georgopoulos.georgios@gmail.com>
2011-02-22 23:42:15 +0000

    Paint bug in Label fixed.

--- src/gwt/mosaic/client/beans/BeanAdapterFactory.java
+++ src/gwt/mosaic/client/beans/BeanAdapterFactory.java
@@ -1,27 +1,19 @@
 package gwt.mosaic.client.beans;
 
-import com.google.gwt.core.client.GWT;
-
 import gwt.mosaic.client.collections.HashMap;
 import gwt.mosaic.client.collections.Map;
 
 public abstract class BeanAdapterFactory<T> {
-	private static Map<Class<?>, BeanAdapterFactory<?>> reg;
+	private static final Map<Class<?>, BeanAdapterFactory<?>> beanAdapterFactoryMap = new HashMap<Class<?>, BeanAdapterFactory<?>>();
 
-	public static BeanAdapter<?> createFor(final Object value) {
-			BeanAdapterFactory<?> factory = reg.get(value.getClass());
-			if (factory != null) {
-				return factory.create(value);
-			} else {
-				return null;
-			}
+	public static BeanAdapter<?> createFor(Object value) {
+		BeanAdapterFactory<?> factory = beanAdapterFactoryMap.get(value
+				.getClass());
+		return (factory != null) ? factory.create(value) : null;
 	}
 
 	public static void register(Class<?> beanType, BeanAdapterFactory<?> factory) {
-		if (reg == null) {
-			reg = new HashMap<Class<?>, BeanAdapterFactory<?>>();
-		}
-		reg.put(beanType, factory);
+		beanAdapterFactoryMap.put(beanType, factory);
 	}
 
 	protected abstract BeanAdapter<T> create(Object value);
