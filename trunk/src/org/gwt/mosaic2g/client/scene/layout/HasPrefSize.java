1e9941f9453c2f4c88e068a9a0135fede7bddbc8
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@15e68a53-2852-0410-b422-3de62d02f58e>
2011-04-11 13:41:36 +0000

    Removed trunk/src, trunk/war, trunk/test

--- trunk/src/org/gwt/mosaic2g/client/scene/layout/HasPrefSize.java
+++
@@ -1,8 +0,0 @@
-package org.gwt.mosaic2g.client.scene.layout;
-
-import org.gwt.mosaic2g.binding.client.Property;
-
-public interface HasPrefSize {
-	Property<Integer> getPrefWidth();
-	Property<Integer> getPrefHeight();
-}
